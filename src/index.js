2284a122b8cd138914beab54e8f7c3a9394e742a
Paul Westerdale <paul@westerdale.me>
2019-07-18 12:54:25 +0100

    include dependencies on build

--- src/index.js
+++ src/index.js
@@ -1,62 +1,66 @@
-import validator from 'validator';
+import validator from 'validator'
 
-validator.isRequired = (value) => {
-    return (value && value !== "");
-};
+validator.isRequired = value => {
+    return value && value !== ''
+}
 
 const basicLang = (check, field) => {
-    switch(check) {
+    switch (check) {
         case 'isRequired':
-            return `${field} is required`;
+            return `${field} is required`
         case 'isEmail':
-            return `${field} must be a valid E-mail address`;
+            return `${field} must be a valid E-mail address`
         default:
-            return `${field} must be a valid ${check.replace('is', '').toLowerCase()}`;
+            return `${field} must be a valid ${check
+                .replace('is', '')
+                .toLowerCase()}`
     }
 }
 
 function runRules(rules, value, key) {
     const toValidate = rules.split('|')
-    return toValidate.reduce((acc, rule) => {
-        if (validator[rule]) {
-            const ruleResult = validator[rule](value);
-            if(!ruleResult && acc.isValid !== false) {
-                acc.isValid = false;
-                acc.errors.push(basicLang(rule, key));
+    return toValidate.reduce(
+        (acc, rule) => {
+            if (validator[rule]) {
+                const ruleResult = validator[rule](value)
+                if (!ruleResult && acc.isValid !== false) {
+                    acc.isValid = false
+                    acc.errors.push(basicLang(rule, key))
+                }
+                return acc
+            } else {
+                console.log('unable to find validate function')
             }
-            return acc;
-        } else {
-            console.log("unable to find validate function");
-        }
-    }, { isValid : true, errors : [] });
+        },
+        { isValid: true, errors: [] }
+    )
 }
 
 export function isValid(config, values, lang = false) {
     let result = {
-        fields : {},
-        errorFields : {},
+        fields: {},
+        errorFields: {},
         errorMessages: {},
-        isValid : null
-    };
-    for(let k in config) {
-        let ruleResult;
-        if(typeof config[k] === "string") {
-            ruleResult = runRules(config[k], values[k], k);
+        isValid: null,
+    }
+    for (let k in config) {
+        let ruleResult
+        if (typeof config[k] === 'string') {
+            ruleResult = runRules(config[k], values[k], k)
         } else {
-            console.log(typeof config, config);
+            console.log(typeof config, config)
         }
-        result.fields[k] = ruleResult.isValid;
+        result.fields[k] = ruleResult.isValid
         if (ruleResult.isValid === false) {
-            result.errorFields[k] = true;
-            result.errorMessages[k] = [...ruleResult.errors];
+            result.errorFields[k] = true
+            result.errorMessages[k] = [...ruleResult.errors]
         }
     }
-    result.isValid = Object.values(result.fields)
-    .reduce((acc, field) => {
-        if(field === false && acc === true) {
-            acc = false;
+    result.isValid = Object.values(result.fields).reduce((acc, field) => {
+        if (field === false && acc === true) {
+            acc = false
         }
-        return acc;
+        return acc
     }, true)
-    return result;
+    return result
 }
--- test/validate.spec.js
+++ test/validate.spec.js
@@ -1,23 +1,20 @@
-const { isValid } = require('../dist');
+const { isValid } = require('../dist')
 
-describe("Thread Validator", () => {
-
-    test("It should validate a simple object", () => {
+describe('Thread Validator', () => {
+    test('It should validate a simple object', () => {
         const schema = {
-            title : "isRequired",
-            email : "isRequired|isEmail"
+            title: 'isRequired',
+            email: 'isRequired|isEmail',
         }
 
         const result = isValid(schema, {
             title: 'Test',
-            email: 'paul'
-        });
+            email: 'paul',
+        })
 
-        expect(result.errorFields.email).toBe(true);
-        expect(result.fields.title).toBe(true);
-        expect(result.fields.email).toBe(false);
-        expect(result.isValid).toBe(false);
-
-    });
-
-});
+        expect(result.errorFields.email).toBe(true)
+        expect(result.fields.title).toBe(true)
+        expect(result.fields.email).toBe(false)
+        expect(result.isValid).toBe(false)
+    })
+})
